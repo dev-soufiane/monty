24aa391825b8a3b5107a5c6d022cf78a52ea1a54
dev-soufiane <dev.sofyane@gmail.com>
2023-11-16 15:08:51 -0800

    Add _strtok file

---
+++ _strtok.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * _strtok - Splits a string into tokens.
+ * @str: String.
+ * @d: Delimiter.
+ *
+ * Return: First part of the string.
+ */
+
+char *_strtok(char *str, char *d)
+{
+	static char *x;
+	int i = 0, j = 0;
+
+	if (!str)
+		str = x;
+	while (str[i] != '\0')
+	{
+		if (_search(d, str[i]) == 0 && str[i + 1] == '\0')
+		{
+			x = str + i + 1;
+			*x = '\0';
+			str = str + j;
+			return (str);
+		}
+		else if (_search(d, str[i]) == 0 && _search(d, str[i + 1]) == 0)
+			i++;
+		else if (_search(d, str[i]) == 0 && _search(d, str[i + 1]) == 1)
+		{
+			x = str + i + 1;
+			*x = '\0';
+			x++;
+			str = str + j;
+			return (str);
+		}
+		else if (_search(d, s[i]) == 1)
+		{
+			j++;
+			i++;
+		}
+	}
+	return (NULL);
+}
